03f1360926afc52c311e5a3cbf1e3a46f927e2cb
Rizky Wahyu <kynnicoo16@gmail.com>
2026-05-21 10:27:56 +0700

    resolve conflict

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://192.168.1.10:8000/api',
+    defaultValue: 'http://10.10.186.195:8000/api',
   );
 }
